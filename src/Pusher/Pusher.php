d3c4d079131b3973b0c73d093cdf5ea00f069ee4
wcj343169893 <wcj343169893@163.com>
2018-06-05 17:26:03 +0800

    修改bug

--- src/Pusher/Pusher.php
+++ src/Pusher/Pusher.php
@@ -12,10 +12,10 @@ class Pusher
     protected $production_mode = false;
 
 
-    public function __construct($appKey, $masterSecret){
+    public function __construct($appKey, $masterSecret,$production_mode=false){
         $this->appKey = $appKey;
         $this->appMasterSecret = $masterSecret;
         $this->timestamp = strval(time());
-        $this->production_mode = config('umeng.production_mode');
+        $this->production_mode = $production_mode;
     }
 }
--- src/Pusher/UmengPusher.php
+++ src/Pusher/UmengPusher.php
@@ -1,26 +1,48 @@
 <?php
-
 namespace UmengPusher\Umeng\Pusher;
 
 class UmengPusher
 {
+    
     private $android = null;
+    
     private $ios = null;
-    public function __construct() {
-        $iosAppKey = config('umeng.ios_app_key');
-        $iosAppMasterSecret = config('umeng.ios_app_master_secret');
-        $androidAppKey = config('umeng.android_app_key');
-        $androidAppMasterSecret = config('umeng.android_app_master_secret');
-
-        $this->android = new AndroidPusher($androidAppKey, $androidAppMasterSecret);
-        $this->ios = new IOSPusher($iosAppKey, $iosAppMasterSecret);
+    
+    /**
+     *
+     * @param array $settings
+     * [
+     *  "ios"=>[
+     *      "app_key"=>"aaa",
+     *      "app_master_secret"=>"bbb",
+     *  ],
+     *  "android"=>[
+     *      "app_key"=>"aaa",
+     *      "app_master_secret"=>"bbb",
+     *  ]
+     * ]
+     */
+    public function __construct($settings = [])
+    {
+        if (! empty($settings["ios"])) {
+            $iosAppKey = $settings["ios"]['app_key'];
+            $iosAppMasterSecret = $settings["ios"]['app_master_secret'];
+            $this->ios = new IOSPusher($iosAppKey, $iosAppMasterSecret,$settings["product"]);
+        }
+        if (! empty($settings["android"])) {
+            $androidAppKey = $settings["android"]['app_key'];
+            $androidAppMasterSecret = $settings["android"]['app_master_secret'];
+            $this->android = new AndroidPusher($androidAppKey, $androidAppMasterSecret,$settings["product"]);
+        }
     }
-
-    public function android(){
+    
+    public function android()
+    {
         return $this->android;
     }
-
-    public function ios(){
+    
+    public function ios()
+    {
         return $this->ios;
     }
 }
